0f28a37e200767444230f9b86287bfe4a7218602
fairytail655 <2623538943@qq.com>
2020-08-10 11:46:42 +0800

    a

--- models/resnet.py
+++ models/resnet.py
@@ -147,12 +147,12 @@ class ResNet(nn.Module):
                 transforms.RandomCrop(32, padding=4),  #先四周填充0，在吧图像随机裁剪成32*32
                 transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
                 transforms.ToTensor(),
-                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), #R,G,B每层的归一化用到的均值和方差
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)), #R,G,B每层的归一化用到的均值和方差
                                 # normalize
             ]),
             'eval': transforms.Compose([
                 transforms.ToTensor(),
-                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
                 # normalize
             ])
         }    
