c3f290e5a80a030125b664e6c8d152e42b1f1370
fairytail655 <2623538943@qq.com>
2020-08-12 10:30:14 +0800

    a

--- models/test.py
+++ models/test.py
@@ -1,3 +1,12 @@
-import platform
+import numpy as np
+import matplotlib.pyplot as plt
 
-print(platform.system())
+n = 1
+k = 1
+x = np.arange(-10, 10, 0.01)
+y = np.tanh(n*(x-k))
+
+plt.plot(x, y)
+plt.show()
+
+print(y)
